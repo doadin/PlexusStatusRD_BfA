d21f4d9c1721d4f8748a192dffc84af5291634f7
Unknown <tbkizle@gmail.com>
2018-08-11 00:27:56 -0400

    Initial template

---
+++ Uldir.lua
@@ -0,0 +1,48 @@
+-- local zone = "Uldir"
+local zoneid = 1148
+
+-- Trash
+
+
+-- Varimathras
+GridStatusRaidDebuff:BossNameId(zoneid, 10, "Varimathras")
+GridStatusRaidDebuff:DebuffId(zoneid, 244042, 11, 6, 6, false, true) -- Marked Prey
+
+-- Aggramar
+GridStatusRaidDebuff:BossNameId(zoneid, 20, "Aggramar")
+
+
+-- Portal Keeper Hasabel
+GridStatusRaidDebuff:BossNameId(zoneid, 40, "Portal Keeper Hasabel")
+
+
+-- Noura, Mother of Flames <Coven of Shivarra>
+GridStatusRaidDebuff:BossNameId(zoneid, 60, "Noura")
+
+-- F'harg
+GridStatusRaidDebuff:BossNameId(zoneid, 80, "F'harg")
+
+
+-- Garothi Worldbreaker
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Garothi Worldbreaker")
+
+
+-- Admiral Svirax
+GridStatusRaidDebuff:BossNameId(zoneid, 110, "Admiral Svirax")
+
+
+-- Kin'garoth <Engineer of the Apocalypse>
+GridStatusRaidDebuff:BossNameId(zoneid, 120, "Kin'garoth")
+
+
+-- Imonar the Soulhunter
+GridStatusRaidDebuff:BossNameId(zoneid, 130, "Imonar the Soulhunter")
+
+
+-- Essence of Eonar
+GridStatusRaidDebuff:BossNameId(zoneid, 150, "Essence of Eonar")
+
+
+-- Argus the Unmaker
+GridStatusRaidDebuff:BossNameId(zoneid, 170, "Argus the Unmaker")
+

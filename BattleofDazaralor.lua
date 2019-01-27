02ca98edad8cafb59638162ab4d877aa905788ab
Unknown <tbkizle@gmail.com>
2019-01-26 19:57:02 -0500

    Start Battle of Dazar'alor Debuffs

---
+++ BattleofDazaralor.lua
@@ -0,0 +1,72 @@
+-- local zone = "Battle of Dazar'alor"
+local zoneid = 1358
+
+-- Trash
+
+-- Champion of the Light
+--GridStatusRaidDebuff:BossNameId(zoneid, 10, "Champion of the Light")
+GridStatusRaidDebuff:DebuffId(zoneid, 289512, 1, 5, 5, true) -- Molten Punch
+GridStatusRaidDebuff:DebuffId(zoneid, 283651, 1, 5, 5, true) -- Blinding Faith
+GridStatusRaidDebuff:DebuffId(zoneid, 289412, 1, 5, 5, true) -- Bestial Impact
+GridStatusRaidDebuff:DebuffId(zoneid, 287437, 1, 5, 5, true) -- Frozen Beam
+GridStatusRaidDebuff:DebuffId(zoneid, 287294, 1, 5, 5, true) -- Blizzard
+GridStatusRaidDebuff:DebuffId(zoneid, 284645, 1, 5, 5, true) -- Topaz of Brilliant Sunlight
+GridStatusRaidDebuff:DebuffId(zoneid, 285014, 1, 5, 5, true) -- Coin Shower
+GridStatusRaidDebuff:DebuffId(zoneid, 284567, 1, 5, 5, true) -- Tailwind Sapphire
+GridStatusRaidDebuff:DebuffId(zoneid, 289645, 1, 5, 5, true) -- Polymorph
+GridStatusRaidDebuff:DebuffId(zoneid, 284519, 1, 5, 5, true) -- Quickened Pulse
+GridStatusRaidDebuff:DebuffId(zoneid, 285998, 1, 5, 5, true) -- Ferocious Roar
+GridStatusRaidDebuff:DebuffId(zoneid, 287325, 1, 5, 5, true) -- Comet Storm
+GridStatusRaidDebuff:DebuffId(zoneid, 288051, 1, 5, 5, true) -- Dragon's Breath
+GridStatusRaidDebuff:DebuffId(zoneid, 284453, 1, 5, 5, true) -- Chi-Ji's Song
+GridStatusRaidDebuff:DebuffId(zoneid, 283063, 1, 5, 5, true) -- Flames of Punishment
+GridStatusRaidDebuff:DebuffId(zoneid, 253562, 1, 5, 5, true) -- Wildfire
+GridStatusRaidDebuff:DebuffId(zoneid, 285479, 1, 5, 5, true) -- Flame Jet
+GridStatusRaidDebuff:DebuffId(zoneid, 284883, 1, 5, 5, true) -- Unleashed Rage
+GridStatusRaidDebuff:DebuffId(zoneid, 284374, 1, 5, 5, true) -- Magma Trap
+GridStatusRaidDebuff:DebuffId(zoneid, 284556, 1, 5, 5, true) -- Shadow-Touched
+GridStatusRaidDebuff:DebuffId(zoneid, 285671, 1, 5, 5, true) -- Crushed
+GridStatusRaidDebuff:DebuffId(zoneid, 289471, 1, 5, 5, true) -- Terrifying Roar
+GridStatusRaidDebuff:DebuffId(zoneid, 284527, 1, 5, 5, true) -- Diamond of the Unshakeable Protector
+GridStatusRaidDebuff:DebuffId(zoneid, 284817, 1, 5, 5, true) -- Earthen Roots
+GridStatusRaidDebuff:DebuffId(zoneid, 284424, 1, 5, 5, true) -- Scorching Ground
+GridStatusRaidDebuff:DebuffId(zoneid, 289514, 1, 5, 5, true) -- Molten Ground
+GridStatusRaidDebuff:DebuffId(zoneid, 289292, 1, 5, 5, true) -- Bestial Throw
+GridStatusRaidDebuff:DebuffId(zoneid, 283573, 1, 5, 5, true) -- Sacred Blade
+GridStatusRaidDebuff:DebuffId(zoneid, 287456, 1, 5, 5, true) -- Frost Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 284814, 1, 5, 5, true) -- Emerald of Earthen Roots
+GridStatusRaidDebuff:DebuffId(zoneid, 284664, 1, 5, 5, true) -- Incandescence
+GridStatusRaidDebuff:DebuffId(zoneid, 287424, 1, 5, 5, true) -- Thief's Bane
+GridStatusRaidDebuff:DebuffId(zoneid, 288151, 1, 5, 5, true) -- Tested
+GridStatusRaidDebuff:DebuffId(zoneid, 289307, 1, 5, 5, true) -- Bestial Throw Target
+GridStatusRaidDebuff:DebuffId(zoneid, 284881, 1, 5, 5, true) -- Opal of Unleashed Rage
+GridStatusRaidDebuff:DebuffId(zoneid, 287993, 1, 5, 5, true) -- Chilling Touch
+GridStatusRaidDebuff:DebuffId(zoneid, 285659, 1, 5, 5, true) -- Apetagonizer Core
+GridStatusRaidDebuff:DebuffId(zoneid, 286740, 1, 5, 5, true) -- Light's Fury
+GridStatusRaidDebuff:DebuffId(zoneid, 284470, 1, 5, 5, true) -- Hex of Lethargy
+GridStatusRaidDebuff:DebuffId(zoneid, 284493, 1, 5, 5, true) -- Pulse-quickening Toxin
+GridStatusRaidDebuff:DebuffId(zoneid, 289484, 1, 5, 5, true) -- Burning Bite
+GridStatusRaidDebuff:DebuffId(zoneid, 284472, 1, 5, 5, true) -- Deadly Hex
+GridStatusRaidDebuff:DebuffId(zoneid, 283609, 1, 5, 5, true) -- Crush
+GridStatusRaidDebuff:DebuffId(zoneid, 286501, 1, 5, 5, true) -- Creeping Blaze
+GridStatusRaidDebuff:DebuffId(zoneid, 283582, 1, 5, 5, true) -- Consecration
+GridStatusRaidDebuff:DebuffId(zoneid, 283069, 1, 5, 5, true) -- Megatomic Fire
+GridStatusRaidDebuff:DebuffId(zoneid, 287556, 1, 5, 5, true) -- Soul Barrage
+GridStatusRaidDebuff:DebuffId(zoneid, 289772, 1, 5, 5, true) -- Impale
+GridStatusRaidDebuff:DebuffId(zoneid, 285632, 1, 5, 5, true) -- Stalking
+GridStatusRaidDebuff:DebuffId(zoneid, 287072, 1, 5, 5, true) -- Liquid Gold
+GridStatusRaidDebuff:DebuffId(zoneid, 282010, 1, 5, 5, true) -- Shattered
+GridStatusRaidDebuff:DebuffId(zoneid, 284573, 1, 5, 5, true) -- Tailwinds
+GridStatusRaidDebuff:DebuffId(zoneid, 286503, 1, 5, 5, true) -- Beam
+GridStatusRaidDebuff:DebuffId(zoneid, 284546, 1, 5, 5, true) -- Depleted Diamond
+GridStatusRaidDebuff:DebuffId(zoneid, 283507, 1, 5, 5, true) -- Volatile Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 284558, 1, 5, 5, true) -- Amethyst of the Shadow King
+GridStatusRaidDebuff:DebuffId(zoneid, 287295, 1, 5, 5, true) -- Chilled
+GridStatusRaidDebuff:DebuffId(zoneid, 286988, 1, 7, 7, true, true) -- Searing Embers
+GridStatusRaidDebuff:DebuffId(zoneid, 284595, 1, 5, 5, true) -- Penance
+GridStatusRaidDebuff:DebuffId(zoneid, 287747, 1, 5, 5, true) -- Force Orb
+GridStatusRaidDebuff:DebuffId(zoneid, 282037, 1, 5, 5, true) -- Rising Flames
+GridStatusRaidDebuff:DebuffId(zoneid, 287460, 1, 5, 5, true) -- Heart of Frost
+GridStatusRaidDebuff:DebuffId(zoneid, 283617, 1, 5, 5, true) -- Wave of Light
+GridStatusRaidDebuff:DebuffId(zoneid, 285875, 1, 5, 5, true) -- Rending Bite
+
